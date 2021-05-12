c0ad96b4eee10aaf4b0c87607355cd5ff3cd64d7
Nolan Darilek <nolan@thewordnerd.info>
2021-05-12 10:53:44 -0500

    Configure client on startup.

--- src/lib.rs
+++ src/lib.rs
@@ -23,6 +23,13 @@ impl Default for NakamaConfig {
     }
 }
 
+fn setup(config: Res<Option<NakamaConfig>>, mut client: NonSendMut<Option<ApiClient>>) {
+    if let Some(config) = &*config {
+        let api_client = ApiClient::new(&config.key, &config.server, config.port, &config.protocol);
+        *client = Some(api_client);
+    }
+}
+
 fn config_changed(config: Res<Option<NakamaConfig>>, mut client: NonSendMut<Option<ApiClient>>) {
     if config.is_changed() {
         if let Some(config) = &*config {
@@ -43,7 +50,9 @@ pub struct NakamaPlugin;
 
 impl Plugin for NakamaPlugin {
     fn build(&self, app: &mut bevy::prelude::AppBuilder) {
-        app.init_resource::<Option<NakamaConfig>>().init_non_send_resource::<Option<ApiClient>>()
+        app.init_resource::<Option<NakamaConfig>>()
+            .init_non_send_resource::<Option<ApiClient>>()
+            .add_startup_system(setup.system())
             .add_system(config_changed.system())
             .add_system(tick.system());
     }
